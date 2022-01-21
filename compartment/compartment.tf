db7228cb28f156c26fe66a6bbc961f9b65b610c5
DurgaPhanindra Nallam <durgaphanindra.nallam@gmail.com>
2022-01-21 08:09:17 +0530

    first tf compartment

---
+++ compartment/compartment.tf
@@ -0,0 +1,7 @@
+resource "oci_identity_compartment" "tf-compartment"{
+
+compartment_id="ocid1.tenancy.oc1..aaaaaaaau4b6hrojtcurmrg55lf3qfzl2yl6jjifpvfvrclx5nlw2jjv67sq"
+description= "OML traing"
+name = "OML"
+
+}
